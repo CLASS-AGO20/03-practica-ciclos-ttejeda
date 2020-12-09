1c22c09778f8ed289e8c3208bcadd001cc5cd70b
Tirzo <ttejeda@ucol.mx>
2020-12-09 15:47:23 -0600

    implemented factorial()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-export default class App {}
+export default class App {
+
+    factorial(numero){
+        let final = 1;
+
+        for(let i = 1; i <= numero; i = i + 1){
+            final = final * i;
+        }
+
+      return final;
+    }
+}
 
 let app = new App();
+
+console.log(app.factorial(6));
